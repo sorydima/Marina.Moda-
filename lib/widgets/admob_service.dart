aec1cfacae29e8bf1b7b3393f3378d60721cba59
Dmitry Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2024-05-28 18:55:03 +0300

    Update admob_service.dart

--- lib/widgets/admob_service.dart
+++ lib/widgets/admob_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import '../helpers/Constant.dart';
+import 'package:marinamoda/helpers/Constant.dart';
 
 class AdMobService {
   static String get bannerAdId =>
